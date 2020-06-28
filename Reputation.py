a0a98512f8bc9d667de074683a3eea5bec2ecca0
Caleb Richardson <richardsoncaleb60@gmail.com>
2020-06-28 02:38:13 -0400

    seperated classes into its own file

--- Reputation.py
+++ Reputation.py
@@ -6,6 +6,7 @@ from urllib.request import urlopen as uReq
 import asyncio
 import ast
 from bs4 import BeautifulSoup as soup
+from Bot_Classes import *
 
 #things to get setup with google, being authorized and whatnot
 scope = [
@@ -30,46 +31,6 @@ discord_client = commands.Bot(command_prefix=prefix)
 #discord_client.case_insensitive = True
 #removing default help command
 discord_client.remove_command('help')
-
-class Game:
-    def __init__(self, Data : list):
-        self.Owner = str(Data[0])
-        self.FullName = str(Data[1])
-        self.HoursPlayed = str(Data[2])
-        self.SteamID = str(Data[3])
-        self.StorePage = str(Data[4])
-        self.Multiplayer = str(Data[5])
-        self.Downloaded = str(Data[6])
-        self.Nickname = str(Data[7])
-
-    def Format_Details(self, formatting):
-        Possible_formats = ['f','n','a','h','s','o','d']
-        formatted_details = ''
-        if 'n' in formatting:
-            name_type = self.Nickname
-        else:
-            name_type = self.FullName
-
-        if len(formatting) > 1:
-            for char in formatting[1::]:
-                if char in Possible_formats:
-                    if char == 'a':
-                        formatted_details += 'Hours: '+self.HoursPlayed+'\n'+'Steam store link: '+self.StorePage+'\n'+'Online: '+self.Multiplayer+'\n'+'Downloaded: '+self.Downloaded
-                    else:
-                        if char == 'h':
-                            formatted_details += 'Hours: ' + self.HoursPlayed
-                            formatted_details += '\n'
-                        if char == 's':
-                            formatted_details += 'Steam store link: ' + self.StorePage
-                            formatted_details += '\n'
-                        if char == 'o':
-                            formatted_details += 'Online: ' + self.Multiplayer
-                            formatted_details += '\n'
-                        if char == 'd':
-                            formatted_details += 'Downloaded: ' + self.Downloaded
-                            formatted_details += '\n'
-                    Possible_formats.remove(char)
-        return name_type, formatted_details
 
 #Setting the help command to be what the bot is playing 
 @discord_client.event
