a104272dae4b36764c75c8c3929aab40a4e2d178
Caleb Richardson <richardsoncaleb60@gmail.com>
2020-06-28 01:20:09 -0400

    comments and fixed readLib output without extra formatting

--- Reputation.py
+++ Reputation.py
@@ -50,24 +50,25 @@ class Game:
         else:
             name_type = self.FullName
 
-        for char in formatting[1::]:
-            if char in Possible_formats:
-                if char == 'a':
-                    formatted_details += 'Hours: '+self.HoursPlayed+'\n'+'Steam store link: '+self.StorePage+'\n'+'Online: '+self.Multiplayer+'\n'+'Downloaded: '+self.Downloaded
-                else:
-                    if char == 'h':
-                        formatted_details += 'Hours: ' + self.HoursPlayed
-                        formatted_details += '\n'
-                    if char == 's':
-                        formatted_details += 'Steam store link: ' + self.StorePage
-                        formatted_details += '\n'
-                    if char == 'o':
-                        formatted_details += 'Online: ' + self.Multiplayer
-                        formatted_details += '\n'
-                    if char == 'd':
-                        formatted_details += 'Downloaded: ' + self.Downloaded
-                        formatted_details += '\n'
-                Possible_formats.remove(char)
+        if len(formatting) >1:
+            for char in formatting[1::]:
+                if char in Possible_formats:
+                    if char == 'a':
+                        formatted_details += 'Hours: '+self.HoursPlayed+'\n'+'Steam store link: '+self.StorePage+'\n'+'Online: '+self.Multiplayer+'\n'+'Downloaded: '+self.Downloaded
+                    else:
+                        if char == 'h':
+                            formatted_details += 'Hours: ' + self.HoursPlayed
+                            formatted_details += '\n'
+                        if char == 's':
+                            formatted_details += 'Steam store link: ' + self.StorePage
+                            formatted_details += '\n'
+                        if char == 'o':
+                            formatted_details += 'Online: ' + self.Multiplayer
+                            formatted_details += '\n'
+                        if char == 'd':
+                            formatted_details += 'Downloaded: ' + self.Downloaded
+                            formatted_details += '\n'
+                    Possible_formats.remove(char)
         return name_type, formatted_details
 
 #Setting the help command to be what the bot is playing 
@@ -187,34 +188,50 @@ async def help(ctx, commandName=None):
 
     elif commandName == 'readLib':
         helpEmbed = discord.Embed(title = 'In depth help for ', color = discord.Color.orange())
-        helpEmbed.add_field(name = commandName, value = 'Allows you and others to read the games you have installed.\n\nThis command has one manditory command: username and one optional command: formatting\nSpecify your username or another person\'s in the server to read the users library of games\nThe formatting command allows your to read more or less details of the library of the user you specify. To do this you must put a \'-\' then put any number of and combination of the letters \'f\' \'n\' \'l\' \'o\'.\n\'f\': will make the full game\'s name part of the bot\'s output of the library.\n\'n\': will make the game\'s nickname part of the bot\'s output of the library.\n')
+        helpEmbed.add_field(name = commandName, value = 'Allows you and others to read the games you have installed.\n\nThis command has one manditory command: username and one optional command: formatting\nSpecify your username or another person\'s in the server to read the users library of games\nThe formatting command allows your to read more or less details of the library of the user you specify. To do this you must put a \'-\' then put any number of and combination of the letters  \'f\' \'n\' \'a\' \'h\' \'s\' \'o\' \'d\'.\n\'f\': will make the full game\'s name part of the bot\'s output of the library.\n\'n\': will make the game\'s nickname part of the bot\'s output of the library.\n')
 
     await ctx.send(embed=helpEmbed)
 
 @discord_client.command() 
 async def echo(ctx, *, msg='echo'):
     await ctx.send(f"""```{msg}```""")
-    await ctx.send("```changes work```")
     
 @discord_client.command()
 async def readLib(ctx, user_mention, formatting=None):
+    #creates empty list to hold all the embeds created to be accessed by the user in discord
     library_embeds = []
-    wks = wb.get_worksheet(0) #open first sheet
+    #open first sheet
+    wks = wb.get_worksheet(0)
+    #gets the whole game data worksheet as a single variable to be accessable with one read 
     current_sheet = wks.get_all_values()
+    #gets the number of games the user has for the program to know when to stop looking for more games
     num_of_games = len(wks.findall(user_mention))
+    #makes the specified user's id useable in the code
     user_mention = user_mention.replace('!', '')
+    #creates an empty embed to be added to later as the infromation is parsed
     LibraryEmbed = discord.Embed(title = user_mention + "'s library", description = "Maximum of 5 games per page." , color = discord.Color.orange())
+    #creates variable to check how many games the program has done to know when to stop looking
     embeded_game_count = 0
-    #checks what kind of formatting the memeber would like; all options include "-fnlo"; any combination may be used e.g. "-oln"
+
+    #defaults to just the downloaded format option 
     if formatting == None:
+        #looks at each row of the whole sheet individually
         for row in current_sheet:
+            #skips the very first row of the sheet to skip the row headers
             if not row == current_sheet[0]:
+                #creates an instance of the game class to make information more readable and accessable
                 item = Game(row)
+                #makes sure the owner of the current game instace is the user that was specified in the command
                 if item.Owner == user_mention:
+                    #increments the amount of games the specified person has to be shown
                     embeded_game_count += 1
+                    #adds a field per game to the embed with the downloaded status
                     LibraryEmbed.add_field(name=item.FullName, value='Downloaded: ' + item.Downloaded, inline=False)
+                    # checks to make sure there is only 5 games per page of the library so it doesnt get overwhelming and the embed cant hold the whole library
                     if embeded_game_count%5 == 0 or num_of_games - embeded_game_count == 0:
+                        #adds the current embed to the embed list
                         library_embeds.append(LibraryEmbed)
+                        #creates new blank embed so it can be added to again
                         LibraryEmbed = discord.Embed(title = user_mention + "'s library", description = "Maximum of 5 games per page." , color = discord.Color.orange())
         await ctx.send(embed=library_embeds[0]) #len(library_embeds)-1])
     
